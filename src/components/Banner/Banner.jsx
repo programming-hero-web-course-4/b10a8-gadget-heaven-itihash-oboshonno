0981408a4971eb594fd76afa4a707f697c23b463
Abrar <ahmedabrararian@gmail.com>
2024-11-04 10:37:47 +0600

    Updated Banner Button Animation

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
             <div className="mx-5 rounded-[32px] bg-primPink grid gap-10 justify-center pt-32 pb-40 md:pb-64 px-5 relative bottom-16">
                 <h2 className="text-white text-2xl md:text-6xl font-bold text-center max-w-6xl">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h2>
                 <p className="text-white text-sm md:text-base text-center max-w-3xl mx-auto">Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
-                <button className="font-bold text-lg md:text-xl text-primPink bg-white rounded-full px-5 py-3 mx-auto">Shop Now</button>
+                <button className="font-bold text-lg md:text-xl text-primPink bg-white rounded-full px-5 py-3 mx-auto border-2 border-white hover:bg-transparent hover:border-2 hover:border-white hover:text-white transition-colors">Shop Now</button>
             </div>
             <div className='mx-10'>
                 <div className='max-w-[1100px] mx-auto rounded-3xl p-5 border-2 border-white bg-white bg-opacity-20 relative bottom-44 md:bottom-64'>
